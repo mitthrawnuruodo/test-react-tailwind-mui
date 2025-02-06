cf1295898f8f23f7f2f85a12b905c929d792826a
Lasse Hægland <seilenos@gmail.com>
2025-02-06 09:59:58 +0100

    background

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Card from './components/Card';
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-pink-100 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-teal-50 flex items-center justify-center p-4">
       <Card />
     </div>
   );
